923f431daa08b736578ee927718a76238225b2b4
Diana Volkova <42410297+beyond-godlike@users.noreply.github.com>
2020-06-25 12:44:05 +0300

    Delete LobbyGreetingRepository.kt

--- app/src/main/java/com/unava/dia/mvpsample/lobby/LobbyGreetingRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.unava.dia.mvpsample.lobby
-
-import io.reactivex.Single
-
-class LobbyGreetingRepository {
-    val greeting: Single<String>
-        get() = Single.just("Hello from LobbyGreetingRepository")
-}
